81fa518d9e5525849b731dc1c4f597fbcfd0d539
Jan Doms <jan.doms@gmail.com>
2014-10-16 23:18:56 +0200

    organize code into modules

--- rocks.ml
+++ rocks.ml
@@ -1,97 +1,130 @@
 open Ctypes
 open Foreign
 
+module CBoolean = struct
+  let uchar_true = Unsigned.UChar.of_int 1
+  let uchar_false = Unsigned.UChar.of_int 0
 
-type rocksdb_options_t = unit ptr
-let rocksdb_options_t : rocksdb_options_t typ = ptr void
+  let to_uchar = function
+    | true -> uchar_true
+    | false -> uchar_false
 
-let rocksdb_options_create =
+  let to_int = function
+    | true -> 1
+    | false -> 0
+end
+
+module Options = struct
+  type t = unit ptr
+  let t : t typ = ptr void
+
+  let create =
+    foreign
+      "rocksdb_options_create"
+      (void @-> returning t)
+
+  let _set_create_if_missing =
+    foreign
+      "rocksdb_options_set_create_if_missing"
+      (t @-> uchar @-> returning void)
+
+  let set_create_if_missing t b =
+    _set_create_if_missing t (CBoolean.to_uchar b)
+end
+
+module WriteOptions = struct
+  type t = unit ptr
+  let t : t typ = ptr void
+
+  let create =
+    foreign
+      "rocksdb_writeoptions_create"
+      (void @-> returning t)
+
+  let _set_disable_WAL =
+    foreign
+      "rocksdb_writeoptions_disable_WAL"
+      (t @-> int @-> returning void)
+  let set_disable_WAL t b =
+    _set_disable_WAL t (CBoolean.to_int b)
+end
+
+module ReadOptions = struct
+  type t = unit ptr
+  let t : t typ = ptr void
+
+  let create =
+    foreign
+      "rocksdb_readoptions_create"
+      (void @-> returning t)
+end
+
+module RocksDb = struct
+  type t = unit ptr
+  let t : t typ = ptr void
+
+  let open_db =
+    foreign
+      "rocksdb_open"
+      (Options.t @-> string @-> ptr string_opt @-> returning t)
+
+  let close =
+    foreign
+      "rocksdb_close"
+      (t @-> returning void)
+
+  let put =
+    foreign
+      "rocksdb_put"
+      (t @-> WriteOptions.t @->
+       ocaml_string @-> size_t @-> ocaml_string @-> size_t @->
+       ptr string_opt @-> returning void)
+
+  let get =
+    foreign
+      "rocksdb_get"
+      (t @-> ReadOptions.t @->
+       ocaml_string @-> size_t @-> ptr size_t @->
+       ptr string_opt @->
+       returning (ptr char))
+
+end
+
+type writebatch_t = unit ptr
+let writebatch_t : writebatch_t typ = ptr void
+
+let writebatch_create =
   foreign
-    "rocksdb_options_create"
-    (void @-> returning rocksdb_options_t)
-
-let rocksdb_options_set_create_if_missing =
-  foreign
-    "rocksdb_options_set_create_if_missing"
-    (rocksdb_options_t @-> uchar @-> returning void)
-
-type rocksdb_writeoptions_t = unit ptr
-let rocksdb_writeoptions_t : rocksdb_writeoptions_t typ = ptr void
-
-let rocksdb_writeoptions_create =
-  foreign
-    "rocksdb_writeoptions_create"
-    (void @-> returning rocksdb_writeoptions_t)
-
-let rocksdb_writeoptions_disable_WAL =
-  foreign
-    "rocksdb_writeoptions_disable_WAL"
-    (rocksdb_writeoptions_t @-> int @-> returning void)
-
-type rocksdb_t = unit ptr
-let rocksdb_t : rocksdb_t typ = ptr void
-
-let rocksdb_open =
-  foreign
-    "rocksdb_open"
-    (rocksdb_options_t @-> string @-> ptr string_opt @-> returning rocksdb_t)
-
-let rocksdb_close =
-  foreign
-    "rocksdb_close"
-    (rocksdb_t @-> returning void)
-
-let rocksdb_put =
-  foreign
-    "rocksdb_put"
-    (rocksdb_t @-> rocksdb_writeoptions_t @->
-     ocaml_string @-> size_t @-> ocaml_string @-> size_t @->
-     ptr string_opt @-> returning void)
-
-type rocksdb_readoptions_t = unit ptr
-let rocksdb_readoptions_t : rocksdb_readoptions_t typ = ptr void
-
-let rocksdb_readoptions_create =
-  foreign
-    "rocksdb_readoptions_create"
-    (void @-> returning rocksdb_readoptions_t)
-
-let rocksdb_get =
-  foreign
-    "rocksdb_get"
-    (rocksdb_t @-> rocksdb_readoptions_t @->
-     ocaml_string @-> size_t @-> ptr size_t @->
-     ptr string_opt @->
-     returning (ptr char))
+    "rocksdb_writebatch_create"
+    (void @-> returning writebatch_t)
 
 let () =
-  let options = rocksdb_options_create () in
-  let utrue = Unsigned.UChar.of_int 1 in
-  rocksdb_options_set_create_if_missing options utrue;
+  let options = Options.create () in
+  Options.set_create_if_missing options true;
   let err_pointer = allocate string_opt None in
   let assert_no_error () =
     match !@ err_pointer with
     | None -> ()
     | Some err -> failwith err in
   let db =
-    rocksdb_open
+    RocksDb.open_db
       options
       "aname"
       err_pointer
   in
   assert_no_error ();
-  let write_options = rocksdb_writeoptions_create () in
-  rocksdb_put
+  let write_options = WriteOptions.create () in
+  RocksDb.put
     db write_options
     (ocaml_string_start "mykey") (Unsigned.Size_t.of_int 5)
     (ocaml_string_start "avalue") (Unsigned.Size_t.of_int 6)
     err_pointer;
   assert_no_error ();
   let read key =
-    let read_options = rocksdb_readoptions_create () in
+    let read_options = ReadOptions.create () in
     let res_size = allocate size_t (Unsigned.Size_t.of_int 8) in
     let res =
-      rocksdb_get
+      RocksDb.get
         db read_options
         (ocaml_string_start key) (Unsigned.Size_t.of_int (String.length key))
         res_size err_pointer in
@@ -104,4 +137,4 @@ let () =
   let show_string_option = [%derive.Show: string option] in
   print_endline (show_string_option (read "mykey"));
   print_endline (show_string_option (read "mykey2"));
-  rocksdb_close db
+  RocksDb.close db
