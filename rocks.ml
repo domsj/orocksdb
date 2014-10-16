797cdc8a966685595f01a974e0c6978e88a9fb2e
Jan Doms <jan.doms@gmail.com>
2014-10-16 22:14:15 +0200

    slightly tighter typing, allow setting a few options

--- rocks.ml
+++ rocks.ml
@@ -1,30 +1,49 @@
 open Ctypes
 open Foreign
-open PosixTypes
 
 
 type rocksdb_t = unit ptr
-let rocksdb_t = ptr void
+let rocksdb_t : rocksdb_t typ = ptr void
 type rocksdb_options_t = unit ptr
-let rocksdb_options_t = ptr void
+let rocksdb_options_t : rocksdb_options_t typ = ptr void
 
 let rocksdb_options_create =
   foreign
     "rocksdb_options_create"
     (void @-> returning rocksdb_options_t)
 
+let rocksdb_options_set_create_if_missing =
+  foreign
+    "rocksdb_options_set_create_if_missing"
+    (rocksdb_options_t @-> uchar @-> returning void)
+
+let rocksdb_writeoptions_disable_WAL =
+  foreign
+    "rocksdb_writeoptions_disable_WAL"
+    (rocksdb_options_t @-> int @-> returning void)
+
 let rocksdb_open =
   foreign
     "rocksdb_open"
-    (rocksdb_options_t @-> string @-> ptr string_opt @-> returning rocksdb_t)
+    (rocksdb_options_t @-> string @-> ptr string_opt @-> returning (ptr rocksdb_t))
 
+let rocksdb_close =
+  foreign
+    "rocksdb_close"
+    (ptr rocksdb_t @-> returning void)
 
 let () =
   let options = rocksdb_options_create () in
-  let _db =
+  let utrue = Unsigned.UChar.of_int 1 in
+  rocksdb_options_set_create_if_missing options utrue;
+  let err_pointer = allocate string_opt None in
+  let db =
     rocksdb_open
       options
       "aname"
-      (allocate string_opt None)
+      err_pointer
   in
-  print_endline "test"
+  let () = match !@ err_pointer with
+    | None -> print_endline "no error"
+    | Some e -> print_endline e in
+  rocksdb_close db
