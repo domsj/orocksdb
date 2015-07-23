6bf69d5b8c99ff1ff955df708801c412877d5ec0
Romain Slootmaekers <romain.slootmaekers@cloudfounders.com>
2015-07-23 19:36:36 +0200

    split out version & use predefined targets

--- install_rocksdb.sh
+++ install_rocksdb.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
+set -x
+echo $(gcc --version)
 
-shared_lib_file="/usr/local/lib/librocksdb.so"
+VERSION=3.12
+shared_lib_file="/usr/local/lib/librocksdb.so.${VERSION}"
 if [ -e $shared_lib_file ]; then
     echo "$shared_lib_file exists"
 else
     echo "cloning, building, installing rocksdb"
     git clone https://github.com/facebook/rocksdb/
     cd rocksdb
-    git checkout tags/rocksdb-3.12
+    git checkout tags/rocksdb-${VERSION}
     make shared_lib
-    sudo cp ./librocksdb.so "$shared_lib_file"
+    sudo make uninstall
+    sudo make install-shared
+    sudo ldconfig
 fi
